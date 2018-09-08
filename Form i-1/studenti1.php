ed58053ce4c86a95e64934ef4558ebe862067976
sachuuuu <sachithasa@gmail.com>
2018-09-08 19:24:40 +0530

    form-f1-final

--- Form i-1/studenti1.php
+++ Form i-1/studenti1.php
@@ -1,5 +1,7 @@
 <?php
 
+	    include 'SPM-sprint-master/Student Registration/DBConnection/dbcon.php';
+	
 	//Declaring variables for html fields
 		$id = $name = $address = $hphone = $mphone = $email = "";
 		$cgpa = $semester = $year = 0;
@@ -17,8 +19,7 @@
 			$email = $_REQUEST['email'];
 			$semester = $_REQUEST['semester'];
 			$year = $_REQUEST['year'];
-			
-			
+			$cgpa = $_REQUEST['cgpa'];		
 		
 		}
 		
@@ -26,15 +27,33 @@
 	class studenti1{
 		
 		//Save data in DB
-		function storeData($test){
-			echo $test;
-			//DB code here
-		}
+		function storeData($id, $name, $address,$hphone,$mphone,$email,$semester,$year,$cgpa){
+			
+			if(!empty($id) && !empty($name) && !empty($address) && !empty($hphone) && !empty($mphone) && !empty($email) && !empty($cgpa)){
+				$conn = dbCOn();
 
+				$sql = "INSERT INTO formi1 (SID, S_name, S_address, S_home_phone, S_mobile_phone, S_email,semester,year,CGPA)
+						VALUES ('$id', '$name', '$address','$hphone','$mphone','$email','$semester','$year','$cgpa')";
+
+				if ($conn->query($sql) === TRUE) {
+					echo "<script type='text/javascript'>alert('New Record Created Successfully');</script>";
+					 header('Location: https://www.sliit.lk/');
+				} else {
+					echo "Error: " . $sql . "<br>" . $conn->error;
+					echo "<script type='text/javascript'>alert('Error: '.$sql.'<br>'.$conn->error.'');</script>";
+				}
+	
+				$conn->close();
+				}
+				else{
+					 echo "<script type='text/javascript'>alert('Fill all the fields');</script>";
+				}
+		
+			}
 	}
 	
 	$stuObj=new studenti1;
-	$stuObj->storeData($id);
-	mail("sachithasa@gmail.com","gg","gggg");
+	$stuObj->storeData($id, $name, $address,$hphone,$mphone,$email,$semester,$year,$cgpa);
+	
 ?>
 
--- Form i-1/supervisori1.php
+++ Form i-1/supervisori1.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+
+<body>
 <?php
 
+
+	include 'SPM-sprint-master/Student Registration/DBConnection/dbcon.php';
+	
 	//Declaring variables for html fields
 	$id = $ename = $sname = $eaddress = $saddress = $sphone = $stitle = $semail = $sdate = $edate = $date = $task = $learn = $esname = "";
 	$cgpa = $semester = $year = 0;
@@ -23,6 +31,41 @@
 		$esname = $_REQUEST['external_supervisor'];
 		
 	}
+	
+	class supervisori1{
+		
+		//Save data in DB
+		function storeData($id, $ename,$sname,$eaddress,$sphone,$stitle,$semail,$sdate,$edate,$date,$task,$learn,$esname){
+			
+			if(!empty($id) && !empty($ename) && !empty($sname) && !empty($eaddress) && !empty($sphone) && !empty($stitle) && !empty($semail) && 
+						!empty($sdate) && !empty($edate) && !empty($date) && !empty($task) && !empty($learn) && !empty($esname)){
+				$conn = dbCOn();
+
+				$sql = "UPDATE formi1 SET E_name='$ename' ,E_address='$eaddress',SU_name='$sname',SU_phone='$sphone',SU_email='$semail',SU_title='$stitle',startDate='$sdate',endDate='$edate',expected='$task',learn='$learn',EXSU_name='$esname',date='$date'
+						WHERE SID='$id'";
+
+				if ($conn->query($sql) === TRUE) {
+					echo "<script type='text/javascript'>alert('New Record Created Successfully');</script>";
+					header('Location: https://www.sliit.lk/');
+				} else {
+					echo "Error: " . $sql . "<br>" . $conn->error;
+					echo "<script type='text/javascript'>alert('Error: '.$sql.'<br>'.$conn->error.'');</script>";
+				}
+	
+				echo "THANK YOU";
+				$conn->close();
+				}
+				else{
+					 echo "<script type='text/javascript'>alert('Fill all the fields');</script>";
+				}
+		
+			}
+	}
+	
+	$supObj=new supervisori1;
+	$supObj->storeData($id, $ename,$sname,$eaddress,$sphone,$stitle,$semail,$sdate,$edate,$date,$task,$learn,$esname);
 
 ?>
+</body>
+</html>
 
