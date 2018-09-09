49d71b16751b130fc9a6e254e5e63298045e4552
sachuuuu <sachithasa@gmail.com>
2018-09-09 11:14:46 +0530

    home

---
+++ I-6_submission/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- cdn script imports-->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- cdn css import -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- custom script and css import -->
+    <script type="text/javascript" src="js/main/script.js"></script>
+    <link rel="stylesheet" href="css/main/style.css" type="text/css">
+
+    <title>Home</title>
+  </head>
+  <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="#">Student Registration</a>
+           </div>
+           <div class="col-md-6">
+           <a href="#">Fill I-3 Form</a>
+           </div>
+           <div class="col-md-6">
+           <a href="#">Examinar Schedules Viva</a>
+           </div>
+           <div class="col-md-6">
+           <a href="#">Send I-6 Document</a>
+           </div>
+           <div class="col-md-6">
+           <a href="#">Send I-1 Form Document</a>
+           </div>
+           <div class="col-md-6">
+           <a href="#">Send I-3 Form Document</a>
+           </div>
+           <div class="col-md-6">
+           <a href="#">Fill I-3 Form</a>
+           </div>
+        </div>
+      </div>
+  </body>
+</html>
